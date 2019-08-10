bd9676d8b6b45e0202b1b6eafa8a42a067dbf688
timotheuslin <timothy.gh.lin@gmail.com>
2019-08-10 00:24:09 -0700

    barely working

--- ipug/cli.py
+++ ipug/cli.py
@@ -2,17 +2,16 @@
 
 """Console script for ipug."""
 import sys
+import time
 import click
 
+from ipug.ipug import main  as ipug_main
 
-@click.command()
-def main(args=None):
-    """Console script for ipug."""
-    click.echo("Replace this message by putting your code into "
-               "ipug.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
 
+#@click.command()
+def main():
+    """main"""
+    return ipug_main()
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
